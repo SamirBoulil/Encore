ea9b44d510f1ca998a6595f066317ef0cc7e2f7d
Samir Boulil <samir.boulil@lengow.com>
2016-09-11 18:26:04 +0200

    Remove compiled files

--- typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="globals/classnames/index.d.ts" />
-/// <reference path="globals/promise/index.d.ts" />
-/// <reference path="modules/debug/index.d.ts" />
-/// <reference path="modules/firebase/index.d.ts" />
-/// <reference path="modules/react-dom/index.d.ts" />
-/// <reference path="modules/react-router/index.d.ts" />
-/// <reference path="modules/react/index.d.ts" />
